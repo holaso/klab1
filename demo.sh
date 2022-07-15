fde18dfc93e562a1d1403a33f0d0a4646ca1f3e4
Horacio Olaso <horacio_olaso@yahoo.com>
2022-07-14 23:28:12 -0300

    bar

--- demo.sh
+++ demo.sh
@@ -8,5 +8,5 @@ printf "\nWaiting for the echo web server service... \n"
 kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/usage.yaml
 sleep 10
 
-printf "\nYou should see 'foo' as a reponse below (if you do the ingress is working):\n"
-curl http://localhost/foo
+printf "\nYou should see 'bar' as a reponse below (if you do the ingress is working):\n"
+curl http://localhost/bar
